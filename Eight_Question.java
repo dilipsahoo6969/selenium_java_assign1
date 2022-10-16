544875127b71f763191fb27d71a8633b153bd953
dilipsahoo6969 <84728419+dilipsahoo6969@users.noreply.github.com>
2022-10-16 12:26:11 +0530

    Add files via upload

---
+++ Eight_Question.java
@@ -0,0 +1,14 @@
+package Assignment1;
+
+public class Eight_Question {
+
+	public static void main(String[] args) {
+		
+		int [] marks = {78, 12, 89, 55, 35};
+		for(int j = 0; j<5; j++) {
+			if(marks[j] > 80) {
+				System.out.println(marks[j]);
+				}
+		}
+	}
+}
